a6f4eda9a7f1ab49b5f0cbbbbb22ff536182bd5c
Tony0812-dev <2058429024@qq.com>
2025-04-04 20:09:59 +0800

    mod

--- tools/build.sh
+++ tools/build.sh
@@ -18,13 +18,6 @@ cp build/libs/aqua-nightly.jar "$PACK/aqua.jar"
 cp -r config "$PACK/config"
 mkdir -p "$PACK/data"
 
-NOTES="$PACK/ReleaseNotes.md"
-echo '### AquaDX Nightly Release' >> "$NOTES"
-echo "This nightly release is automatically built by github actions on $(date)." >> "$NOTES"
-echo "https://github.com/hykilpikonna/AquaDX" >> "$NOTES"
-echo '### The latest five updates are:' >> "$NOTES"
-git log -"5" --format="- %H %s" | sed '/^$/d' >> "$NOTES"
-
 echo "Compressing"
 cd build/release
 zip -r ../aqua-nightly.zip .
